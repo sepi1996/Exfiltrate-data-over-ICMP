4edd7cf49042e5fc5318f3d71594065b3511f867
sepi1996 <sepi1996@users.noreply.github.com>
2021-11-17 18:15:18 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,47 +1,57 @@
 import socket
 import struct
 import os
+import sys
 #ICMP_CODE = socket.getprotobyname('icmp')
 
-def processData(chunk):
-    print(chunk)
+def processData(chunk, filename):
+    #print(chunk)
     chunk.sort()
+    data = b''
     for n in range(len(chunk)):
-        chunk[n] = chunk[n][4:]
+        a = chunk[n]
+        data = data + a[4:]
+
+    with open("b.jpg", "wb") as writer:
+        writer.write(data)
     exit(0)
 
 
 def main():
-    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    chunk = []
-    while True:
-        raw_data, addr = conn.recvfrom(65536)
-        print(raw_data)
-        dest_mac, src_mac, eth_proto, data = ether_frame(raw_data)
-        print(f'Ether Frame:')
-        print(f'\tDest mac addres {dest_mac}')
-        print(f'\tSource mac addres {src_mac}')
-        print(f'\tProto {eth_proto}')
+    if len(sys.argv) != 2:
+        print("Usage: python3 server.py file")
+    else:
+        filename = str(sys.argv[1])
+        conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+        chunk = []
+        while True:
+            raw_data, addr = conn.recvfrom(65536)
+            print(raw_data)
+            dest_mac, src_mac, eth_proto, data = ether_frame(raw_data)
+            print(f'Ether Frame:')
+            print(f'\tDest mac addres {dest_mac}')
+            print(f'\tSource mac addres {src_mac}')
+            print(f'\tProto {eth_proto}')
 
-        if eth_proto == 8:
-            (version, h_l, ttl, proto, src, target, data) = ipv4_packet(data)
-            print(f'Ipv4 packet')
-            print(f'\t Version: {version}, Header Lenght: {h_l}, TTL: {ttl}')
-            print(f'\t Protocol: {proto}, Source: {src}, Target {target}')
+            if eth_proto == 8:
+                (version, h_l, ttl, proto, src, target, data) = ipv4_packet(data)
+                print(f'Ipv4 packet')
+                print(f'\t Version: {version}, Header Lenght: {h_l}, TTL: {ttl}')
+                print(f'\t Protocol: {proto}, Source: {src}, Target {target}')
 
-            if proto == 1:
-                i_t, code, check, data = icmp_packet(data)
-                print(f'ICMP packet')
-                print(f'\t type: {i_t}, code: {code}, checksum: {check}')
-                print(f'\t data: {data}')
-                print("\n")
-                if int(i_t) == 8:
-                    chunk.append(data)
-                    #print("CHUNK")
-                    #print(chunk)
-                    if data == b'\x7f\xff\xff\xff':
-                        processData(chunk)
-                        
+                if proto == 1:
+                    i_t, code, check, data = icmp_packet(data)
+                    print(f'ICMP packet')
+                    print(f'\t type: {i_t}, code: {code}, checksum: {check}')
+                    print(f'\t data: {data}')
+                    print("\n")
+                    if int(i_t) == 8:
+                        chunk.append(data)
+                        #print("CHUNK")
+                        #print(chunk)
+                        if data == b'\x7f\xff\xff\xff':
+                            processData(chunk, filename)
+
 def ether_frame(data):
     dest_mac, src_mac, eth_proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac(dest_mac), get_mac(src_mac), socket.htons(eth_proto), data[14:]
